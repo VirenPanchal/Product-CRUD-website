face6110f5a3459b905751a6ba3a093fd127e99f
Viren Panchal <panchalviren00@gmail.com>
2024-05-08 19:19:11 -0400

    final code updated

--- src/main/java/com/boostmytool/beststore/models/ProductDto.java
+++ src/main/java/com/boostmytool/beststore/models/ProductDto.java
@@ -12,14 +12,14 @@ public class ProductDto {
 	@NotEmpty(message ="The brand is required")
 	private String brand;
 	
-	@NotEmpty(message="The name is required")
+	@NotEmpty(message ="The category is required")
 	private String catagory;
 	
 	@Min(0)
 	private double price;
 	
-	@Size(min =10,message ="The description should be at least 10 caracters")
-	@Size(min =2000,message ="The description cannot exceed 2000 caracters")
+	@Size(min =1,message ="The description should be at least 10 caracters")
+	@Size(max =2000,message ="The description cannot exceed 2000 caracters")
 	private String description;
 	
 	private MultipartFile imageFile;
@@ -44,7 +44,7 @@ public class ProductDto {
 		return catagory;
 	}
 
-	public void setCategory(String catagory) {
+	public void setCatagory(String catagory) {
 		this.catagory = catagory;
 	}
 
